7ca3499f9a95bf93b5245d736e2a3163ee804ff1
SamEagle <samadeagle@yahoo.com>
2024-11-07 13:29:11 +0330

    Add GeneralSettings model to admin_options.models

--- jewelry_shop/admin_options/models.py
+++ jewelry_shop/admin_options/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class GeneralSettings(models.Model):
+    is_ordering_enabled = models.BooleanField(default=True)
+    is_full_payment_enabled = models.BooleanField(default=True)
+    is_down_payment_enabled = models.BooleanField(default=True)
+    is_maintenance_mode = models.BooleanField(default=False)
+
+    maintenance_message = models.TextField(blank=True, null=True)
+    order_disabled_message = models.TextField(blank=True, null=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    def __str__(self):
+        return "General Settings"
